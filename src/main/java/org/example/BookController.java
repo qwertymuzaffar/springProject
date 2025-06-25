38541263e7ac19ad386fc69e1ca99225b2478ce1
Muzaffar Qosimov <qwertymuzaffar@gmail.com>
2025-06-25 17:20:04 -0400

    @PathVariable annotation

--- src/main/java/org/example/BookController.java
+++ src/main/java/org/example/BookController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class BookController {
 
+    @GetMapping("/books/{id}")
+    public String getBookById(@PathVariable("id") String bookId) {
+        System.out.println("Book ID: " + bookId);
+        return "bookDetails";
+    }
+
     @RequestMapping("/books")
     public String getBooks() {
         return "books";
